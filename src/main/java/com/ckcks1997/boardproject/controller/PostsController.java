afa39aa83fcf359715a5a0df0bc8c530bb1260c5
LCH <ckcks1997@gmail.com>
2021-10-14 22:47:53 +0900

    기능 추가: -글 등록시 BindingResult를 이용한 검증 추가

--- src/main/java/com/ckcks1997/boardproject/controller/PostsController.java
+++ src/main/java/com/ckcks1997/boardproject/controller/PostsController.java
@@ -9,8 +9,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -30,8 +35,20 @@ public class PostsController {
     }
 
     @PostMapping("/posts/save")
-    public String savePosts(PostsDto postsDto){
-        System.out.println("postsDto = " + postsDto);
+    public String savePosts(@ModelAttribute PostsDto postsDto, BindingResult bindingResult){
+        if(!StringUtils.hasText(postsDto.getTitle())){
+            bindingResult.addError(new FieldError("postsDto", "title", "제목은 필수 입니다."));
+        }
+        if(!StringUtils.hasText(postsDto.getAuthor())){
+            bindingResult.addError(new FieldError("postsDto", "author", "작성자 입력은 필수 입니다."));
+        }
+        if(!StringUtils.hasText(postsDto.getContent())){
+            bindingResult.addError(new FieldError("postsDto", "content", "내용을 입력하세요."));
+        }
+        if (bindingResult.hasErrors()) {
+            return "/postsform";
+        }
+
         Posts posts = new Posts(postsDto.getTitle(), postsDto.getContent(), postsDto.getAuthor());
         postsRepository.save(posts);
         return "redirect:/posts/list";
