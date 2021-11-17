bc4ad5d3251650d46b0d1447d4c38dfec5f34055
LCH <ckcks1997@gmail.com>
2021-11-17 11:50:02 +0900

    기능 추가 및 수정: - 페이징 기능 추가(진행중) - Querydsl 도입(진행중)

--- src/main/java/com/ckcks1997/boardproject/domain/posts/PostsRepository.java
+++ src/main/java/com/ckcks1997/boardproject/domain/posts/PostsRepository.java
@@ -3,7 +3,8 @@ package com.ckcks1997.boardproject.domain.posts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface PostsRepository extends JpaRepository<Posts, Long>{
+public interface PostsRepository extends JpaRepository<Posts, Long>
+        , QuerydslPredicateExecutor<Posts>{
 
 
 }
